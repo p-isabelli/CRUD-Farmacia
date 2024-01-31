68c0f5123968acdf5da1fea0b54cdef8436c7d34
isabelli c pereira <isabellipereira58@gmail.com>
2024-01-31 13:31:38 -0300

    CRUD Categoria

--- src/main/java/com/generation/mixfarma/model/Categoria.java
+++ src/main/java/com/generation/mixfarma/model/Categoria.java
@@ -1,17 +1,9 @@
 package com.generation.mixfarma.model;
 
-
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -32,10 +24,6 @@ public class Categoria {
 	@NotBlank (message = " A Descrição da categoria é obrigatória! ")
 	@Size(min = 10, max = 100, message = "A descrição da categoria deve conter no mínimo 10 caracteres e no máximo 100 caracteres!")
 	private String descricao;
-	
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "categoria", cascade = CascadeType.ALL)
-    @JsonIgnoreProperties("categoria")
-    private List<Produto> produtos;
 
 	public Long getIdcat() {
 		return idcat;
@@ -59,13 +47,5 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 }
--- src/main/java/com/generation/mixfarma/repository/CategoriaRepository.java
+++ src/main/java/com/generation/mixfarma/repository/CategoriaRepository.java
@@ -1,6 +1,7 @@
 package com.generation.mixfarma.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,6 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
    public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
    
    public List<Categoria> findAllByCategoriaContainingIgnoreCase (@Param("categoria")String categoria);
+
 
 }
